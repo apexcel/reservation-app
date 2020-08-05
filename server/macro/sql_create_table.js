0c18ea4260a624c27ab235e5eb4b16c9b2fb28a4
oogie826 <oogie826@gmail.com>
2020-08-05 21:29:26 +0900

    Update mongoose connection

--- server/macro/sql_create_table.js
+++ server/macro/sql_create_table.js
@@ -1,4 +1,4 @@
-const db = require('../db');
+const db = require('../database/mysql/mysqlConn');
 
 const COLUMN_LENGTH = 10;
 const connection = db.init();
---
+++ server/macro/test.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+
+node "/home/oogie826/workspace/reservation_app/server/macro/_create_table.js"
