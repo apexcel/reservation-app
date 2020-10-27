bfee85bff75a26a720fd044a1ebb501c5513caaa
oogie826 <oogie826@gmail.com>
2020-10-27 16:10:00 +0900

    Initial commit

--- src/api/SendTo.ts
+++ src/api/SendTo.ts
@@ -2,11 +2,11 @@ import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
     // TODO: dev
-    // baseURL: 'http://127.0.0.1:9000/api',
+    baseURL: 'http://127.0.0.1:9000/api',
     headers: {
         'Access-Control-Allow-Origin': '*'
     },
-    baseURL: 'http://3.35.190.11/api',
+    // baseURL: 'http://3.35.190.11/api',
     // TODO: prod
     // baseURL: 'http://아이피 or DNS : PORT/api',
     // AWS 올릴때는 해당 ip나 주소에 맞게 변경해야함.
